64db2d2349c155193e55a4d77ef6f5b0506e6e45
Gustavo Breternitz <breternitagustavo@gmail.com>
2021-03-04 10:49:02 -0300

    Primeiro commit

---
+++ Buscador_cursos.php
@@ -0,0 +1,18 @@
+<?php
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+require_once "vendor/autoload.php";
+
+
+$cliente = new Client(["base_uri" => "https://www.alura.com.br"]);
+$crawler = new Crawler();
+
+$Buscador = new Buscador($cliente, $crawler);
+$cursos = $Buscador->buscador("/cursos-online-mobile/android");
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
---
+++ src/Buscador.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    private $cliente;
+    private $crawler;
+
+    public function __construct(Client $cliente, Crawler $crawler)
+    {
+
+        $this->cliente = $cliente;
+        $this->crawler = $crawler;
+    }
+
+    public function buscador($url): array
+    {
+        $resposta = $this->cliente->request("GET", $url);
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
+    }
+}
